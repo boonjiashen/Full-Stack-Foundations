d9b91914e2e3cbde87a7caf4fce3e583169ac94f
Jia-Shen Boon <jiashen@gmail.com>
2016-03-13 15:23:21 +0000

    Add 'add/delete' restaurant functionality

--- webserver_flask.py
+++ webserver_flask.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import sessionmaker
 app = Flask(__name__)
 
 # Boiler-plate stuff
-engine=create_engine('sqlite:///Lesson_1/restaurantmenu.db')
+engine=create_engine('sqlite:///restaurantmenu.db')
 Base.metadata.bind=engine
 DBSession=sessionmaker(bind=engine)
 session=DBSession()
@@ -22,15 +22,57 @@ def hola():
 
 @app.route('/restaurants')
 def restaurants():
-    names = [x.name for x in session.query(Restaurant).all()]
-    template = """
+    restaurants = session.query(Restaurant).all()
+    def get_template(restaurant):
+        return """
+                <p>
+                %s<br>
+                <a href="">Edit</a><br>
+                <form action="delete_restaurant" method="post">
+                  <button type="submit" name="rid" value="%d">Delete</button>
+                </form>
+                ---
+                </p>
+                """ % (restaurant.name, restaurant.id)
+    adder = """
     <p>
-    %s<br>
-    <a href="">Edit</a><br>
-    <a href="">Delete</a>
+    <a href="add_restaurant">Add restaurant</a>
     </p>
     """
-    return ''.join([template % name for name in names])
+    return ''.join([get_template(x) for x in restaurants]) + adder
+
+@app.route('/delete_restaurant', methods=['POST'])
+def delete_restaurant():
+    rid = request.form['rid']
+    object = session.query(Restaurant).filter_by(id=rid).first()
+    if not object:
+        return "<h1>Cannot find restaurant with id %s</h1>" % rid
+    name = object.name
+    session.delete(object)
+    session.commit()
+    return 'Successfully deleted restaurant %s with id <br><h1>%s</hl>' % (name, str(rid))
+
+@app.route('/add_restaurant', methods=['GET', 'POST'])
+def add():
+    if request.method == 'GET':
+        form = """
+        Restaurant name to be added
+        <form action="%s" method="POST">
+        <input type="text" name="text">
+        <input type="submit" name="my_form" value="Submit">
+        </form>
+        """ % request.url_rule.rule
+        return form
+    elif request.method == 'POST':
+        restaurant_name = request.form['text']
+        session.add(Restaurant(name=restaurant_name))
+        session.commit()
+        html = """
+        Successfully posted new restaurant
+        <hl>%s</hl>
+        <p><a href="restaurants">Return to restaurant list</a></p>
+        """ % restaurant_name
+        return html
 
 @app.route('/')
 def form():
@@ -50,3 +92,4 @@ def form_post():
 if __name__ == "__main__":
     app.debug = True
     app.run('0.0.0.0', port=8080)
+    #pass
