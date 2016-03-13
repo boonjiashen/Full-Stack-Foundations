14b611dfcb1e6a10f3e45302c909da2a8a468b8c
Jia-Shen Boon <jiashen@gmail.com>
2016-03-13 14:11:11 +0000

    Use Flask to query DB

--- webserver_flask.py
+++ webserver_flask.py
@@ -1,6 +1,16 @@
 from flask import Flask, request
+from database_setup import Restaurant, MenuItem, Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 app = Flask(__name__)
+
+# Boiler-plate stuff
+engine=create_engine('sqlite:///Lesson_1/restaurantmenu.db')
+Base.metadata.bind=engine
+DBSession=sessionmaker(bind=engine)
+session=DBSession()
+
 
 @app.route('/hello')
 def hello_world():
@@ -9,6 +19,18 @@ def hello_world():
 @app.route('/hola')
 def hola():
     return 'hola!'
+
+@app.route('/restaurants')
+def restaurants():
+    names = [x.name for x in session.query(Restaurant).all()]
+    template = """
+    <p>
+    %s<br>
+    <a href="">Edit</a><br>
+    <a href="">Delete</a>
+    </p>
+    """
+    return ''.join([template % name for name in names])
 
 @app.route('/')
 def form():
